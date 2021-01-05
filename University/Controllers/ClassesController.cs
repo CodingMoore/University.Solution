a8726fb6d18384fc9f2165f8a54c0daa513aed01
Randel M <Randel.C.Moore@gmail.com>
2021-01-05 10:06:05 -0800

    complete basic controller route layout
    Co-authored-by: Svea Wade <svealinneawade@gmail.com>
    Co-authored-by: Sarah Gilbert <sarahgilbert064@gmail.com>
    Co-authored-by: Ryland Adams <rylandadams@yahoo.com>

--- University/Controllers/ClassesController.cs
+++ University/Controllers/ClassesController.cs
@@ -1,19 +1,75 @@
-// using Microsoft.AspNetCore.Mvc;
-// using University.Models;
-// using System.Collections.Generic;
-// using System.Linq;
-// using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc;
+using University.Models;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
 
-// namespace University.Controllers
-// {
-//   public class ClassesController : Controller
-//   {
-//     private readonly UniversityContext _db;
-//     public ClassesController(UniversityContext db)
-//     {
-//       _db = db;
-//     }
+namespace University.Controllers
+{
+  public class ClassesController : Controller
+  {
+    private readonly UniversityContext _db;
+    public ClassesController(UniversityContext db)
+    {
+      _db = db;
+    }
 
+    public ActionResult Index()
+    {
+      List<Class> model = _db.Categories.University();
+      return View(model);
+    }
 
-//   }
-// }
+    public ActionResult Create()
+    {
+      return View();
+    }
+
+    [HttpPost]
+    public ActionResult Create(Class class)
+    {
+      _db.Classes.Add(class);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
+
+    public ActionResult Details(int id)
+    {
+      var thisClass = _db.Classes
+          .Include(class => class.Students)
+          .ThenInclude(join => join.Student)
+          .FirstOrDefault(class => class.ClassId == id);
+      return View(thisClass);
+    }
+
+    public ActionResult Edit(int id)
+    {
+      var thisClass = _db.Classes.FirstOrDefault(class => class.ClassId == id);
+      return  View(thisclass);
+    }
+
+    [HttpPost]
+    public ActionResult Edit(Class class)
+    {
+      _db.Entry(class).State = EntityState.Modified;
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
+
+    public ActionResult Delete(int id)
+    {
+      var thisClass = _db.Classes.FirstOrDefault(class => class.ClassId == id);
+      return View(thisClass);
+    }
+
+    [HttpPost, ActionName("Delete")]
+    public ActionResult DeleteConfirmed(int id)
+    {
+      var thisClass = _db.Classes.FirstOrDefault(class => class.ClassId == id);
+      _db.Classes.Remove(thisClass);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
+
+  }
+}
--- University/Controllers/StudentsController.cs
+++ University/Controllers/StudentsController.cs
@@ -1,21 +1,117 @@
-// using Microsoft.AspNetCore.Mvc;
-// using University.Models;
-// using System.Collections.Generic;
-// using System.Linq;
-// using Microsoft.EntityFrameworkCore;
-// using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Mvc;
+using University.Models;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
-// namespace University.Controllers
-// {
-//   public class StudentsController : Controller
-//   {
-//     private readonly UniversityContext _db;
+namespace University.Controllers
+{
+  // [Authorize]
+  public class StudentsController : Controller
+  {
+    private readonly UniversityContext _db;
+    // private readonly UserManager<ApplicationUser> _userManager;
 
-//     public StudentsController(UniversityContext db)
-//     {
-//       _db = db;
-//     }
+    public StudentController(UniversityContext db)
+    {
+      // _userManager = userManager;
+      _db = db;
+    }
 
+    public ActionResult Index()
+    {
+      return View(_db.Items.ToList());
+    }
+
+    public ActionResult Create()
+    {
+      ViewBag.ClassId = new SelectList(_db.Classes, "ClassId", "ClassName");
+      return View();
+    }
+
+    [HttpPost]
+    public async Task<ActionResult> Create(Student student, int ClassId)
+    {
+      // var userId = this.User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+      // var currentUser = await _userManager.FindByIdAsync(userId);
+      // item.User = currentUser;
+      _db.Items.Add(item);
+      if (ClassId != 0)
+      {
+        _db.ClassStudent.Add(new ClassStudent() { ClassId = ClassId, StudentId = student.StudentId });
+      }
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
+
+    public ActionResult Details(int id)
+    {
+      var thisStudent = _db.Students
+          .include(student => student.Classes);
+          .ThenInclude(join => join.Class);
+          .FirstOrDefault(student => student.StudentId == id);
+      return View(thisStudent);
+    }
+
+    public ActionResult Edit(int id)
+    {
+      var thisStudent = _db.Students.FirstOrDefault(student => student.StudentId == id);
+      ViewBag.ClassId = new SelectList(_db.Classes, "ClassId", "ClassName");
+      return View(thisItem);
+    }
     
-//   }
-// }
+    [HttpPost]
+    public ActionResult Edit(Student student int ClassId)
+    {
+      if (ClassId != 0)
+      {
+        _db.ClassStudent.Add(new ClassStudent() { ClassId = ClassId, StudentId = student.StudentId });
+      }
+        _db.Entry(student).State=EntityState.Modified;
+        _db.SaveChanges();
+        return RedirectToAction("Index");
+    }
+
+    public ActionResult AddClass(int id)
+    {
+      var thisStudent = _db.Students.FirstOrDefault(student => students.StudentId == id);
+      ViewBag.ClassId = new SelectList(_db.Classes, "ClassId", "ClassName");
+      return View(thisStudent);
+    }
+    [HttpPost]
+    public ActionResult AddClass(Student student, int ClassId)
+    {
+      if(ClassId != 0)
+      {
+        _db.ClassStudent.Add(new ClassStudent() { ClassId = ClassId, StudentId = student.StudentId});
+      }
+        _db.SaveChanges();
+        return RedirectToAction("Index");
+    }
+
+    public ActionResult Delete(int id)
+      {
+        var thisStudent = _db.Students.FirstOrDefaulr(students => students.StudentId == id);
+        return View(thisStudent);
+      }
+
+      [HttpPost, ActionName("Delete")]
+      public ActionResult DeleteConfirmed(int id)
+      {
+        var thisStudent = _db.Students.FirstOrDefaulr(students => students.StudentId == id);
+        _db.Student.Remove(thisStudent);
+        _db.SaveChanges();
+        return RedirectToAction("Index");
+      }
+    
+    [HttpPost]
+    public ActionResult DeleteClass(int joinId)
+    {
+      var joinEntry = _db.ClassItem.FirstOrDefault(entry => entry.ClassStudentId = joinId);
+      _db.ClassItem.Remove(joinEntry);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
+  }
+}
