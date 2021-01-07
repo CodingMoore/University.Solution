4cafbab2121791492c5fa78666578c33a19a5c5c
Randel M <Randel.C.Moore@gmail.com>
2021-01-07 11:03:09 -0800

    fixed bug with date/time edit field
    Co-authored-by: David Sterry <sterry.david@gmail.com>
    Co-authored-by: Ryland Adams <rylandadams@yahoo.com>
    Co-authored-by: Sarah Gilbert <sarahgilbert064@gmail.com>
    Co-authored-by: Svea Wade <svealinneawade@gmail.com>

--- University/Models/Course.cs
+++ University/Models/Course.cs
@@ -1,4 +1,7 @@
-using System.Collections.Generic; 
+using System.Collections.Generic;
+using System;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace University.Models
 {
@@ -8,9 +11,14 @@ namespace University.Models
         {
             this.JoinEntries = new HashSet<CourseStudent>(); //creating an empty hash set of course students. HashSet is an unordered collection of unique elements. "Studends" would be more accurately named "JoinEntries" since it is a HashSet of CourseStudents.
         }
-
+        [DisplayName("Start Date")]
+        [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:yyyy-MM-dd hh:mm tt}")]
+        public DateTime StartDate { get; set; }
         public int CourseId { get; set; }
+
+        [DisplayName("Course Name")]
         public string CourseName { get; set; }
         public virtual ICollection<CourseStudent> JoinEntries { get; set; } //IColletion is basically a list. The ICollection<T> interface is the base interface for classes in the System.Collections.Generic namespace.
     }
 }
+
